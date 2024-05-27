26608f01efccb5ea0a7c61e2ea1009be0df052f5
MiguelCarachenskiNeto <miguel.carachenski.neto@escola.pr.gov.br>
2024-05-27 09:45:52 -0300

    first commit

--- app.py
+++ app.py
@@ -4,25 +4,52 @@ restaurantes=[{"nome":"Boteco do Chef","categoria":"Bar","ativo":True},
               {"nome":"Comida Caseira","categoria":"Restaurante","ativo":False},
               {"nome":"Kibe Kitchen","categoria":"Árabe","ativo":True}]
 
-def voltar_ao_menu_principal():
-    input("\nDigite uma tecla para voltar ao menu principal: \n")
-    main() 
+def nome_app():
+    #docstring
+    '''
+    Exibe o nome do programa
 
-def mostrar_subtitulo(texto):
-    os.system("clear")
-    print(texto)
-    print()
+    output:
+    - Escreve o nome do programa
 
+    '''
+    print('''
+███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄─▄─██▀▄─██▄─██─▄█▄─▄▄▀██▀▄─██▄─▀█▄─▄█─▄─▄─█▄─▄▄─███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█─▄▄─█
+██─▄─▄██─▄█▀█▄▄▄▄─███─████─▀─███─██─███─▄─▄██─▀─███─█▄▀─████─████─▄█▀████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█─██─█
+▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▀▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀
 
-def desligar_app():
-    mostrar_subtitulo("Finalizando o app")
+                                                █▀▄▀█ █ █▀▀ █░█ █▀▀ █░░                                                
+                                                █░▀░█ █ █▄█ █▄█ ██▄ █▄▄
+''')
 
-def opcao_invalida():
-    os.system("clear")
-    print("Opção inválida\n")
-    voltar_ao_menu_principal()
+def exibir_opcoes():
+    #docstring
+    '''
+    Exibe a lista de opções
+
+    output:
+    - escreve as opcões disponiveis a serem escolhidas
+
+    '''
+    print("1-Cadastrar um restaurante")
+    print("2-Listar restaurantes")
+    print("3-Ativar um restaurante")
+    print("4-Sair do programa\n")
 
 def cadastrar_novo_restaurante():
+    #docstring
+    '''
+    Essa função é responsável por cadastrar um novo restaurante
+
+    input:
+    - nome do restaurante
+    - categoria do restaurante
+
+    output:
+    - adiciona um novo restaurante ao dicionário restaurante
+
+    '''
     os.system("clear")
     nome_do_restaurante=input("digite o nome do novo restaurante: \n")
     os.system("clear")
@@ -32,6 +59,14 @@ def cadastrar_novo_restaurante():
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
+    #docstring
+    '''
+    Mostra os restaurantes cadastrados no dicionario restaurante
+
+    output:
+    - escreve o nome, categoria e status dos restaurantes presentes no dicionário restaurante
+
+    '''
     os.system("clear")
     print("Restaurantes cadastrados:\n")
     for restaurante in restaurantes:
@@ -42,6 +77,17 @@ def listar_restaurantes():
     voltar_ao_menu_principal() 
 
 def ativar_restaurante():
+    #docstring
+    '''
+    Muda o status de um restaurante para ativo ou inativo
+
+    input
+    - nome do restaurante
+
+    output
+    - muda o status do restaurante escolhido
+
+    '''
     mostrar_subtitulo("Alterando o estado do restaurante")
     nome_restaurante=input("digite o nome do restaurante que deseja ativar/desativar:\n")
     restaurante_encontrado = False
@@ -56,22 +102,61 @@ def ativar_restaurante():
         print("O restaurante não foi encontrado")
     voltar_ao_menu_principal()
             
+def desligar_app():
+    #docstring
+    '''
+    Finaliza o programa
 
-def nome_app():
-    print('''
-███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄─▄─██▀▄─██▄─██─▄█▄─▄▄▀██▀▄─██▄─▀█▄─▄█─▄─▄─█▄─▄▄─███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█─▄▄─█
-██─▄─▄██─▄█▀█▄▄▄▄─███─████─▀─███─██─███─▄─▄██─▀─███─█▄▀─████─████─▄█▀████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█─██─█
-▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▀▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀
-''')
+    output:
+    - limpa a exibição
 
-def exibir_opcoes():
-    print("1-Cadastrar um restaurante")
-    print("2-Listar restaurantes")
-    print("3-Ativar um restaurante")
-    print("4-Sair do programa\n")
+    '''
+    os.system("clear")
+    mostrar_subtitulo("Finalizando o app")
+
+def opcao_invalida():
+    #docstring
+    '''
+    Redireciona o usuário ao menu principal caso a opção escolhida seja inválida
+
+    output:
+    - redireciona o usuário ao menu principal
+
+    '''
+    os.system("clear")
+    print("Opção inválida\n")
+    voltar_ao_menu_principal()    
+
+def voltar_ao_menu_principal():
+    input("\nDigite uma tecla para voltar ao menu principal: \n")
+    main() 
+
+def mostrar_subtitulo(texto):
+    #docstring
+    '''
+    Exibe o subtitulo 
+
+    output:
+    - escreve o subtítulo descrito nos parâmetros
+
+    '''
+    os.system("clear")
+    print(texto)
+    print()
 
 def escolher_opcoes():
+    #docstring
+    '''
+    Permite ao usuário executar a opção que deseja
+    
+    input:
+    - opcão escolhida
+
+    output:
+    - executa uma função dependendo da escolha
+
+    '''
+
     try:
         escolha=int(input("Escolha uma opção: "))
         print("Você selecionou a opção:",escolha,"\n")
@@ -91,6 +176,16 @@ def escolher_opcoes():
         opcao_invalida()
 
 def main():
+    #docstring
+    '''
+    Exibe a página principal do programa
+
+    output:
+    - exibe o nome
+    - exibe as opcoes
+    - escolha de opcoes
+
+    '''
     os.system("clear")
     nome_app()
     exibir_opcoes()
